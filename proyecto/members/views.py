669ae773909de31f33b90f308bf1cc05719d849e
César <cesarlpb@gmail.com>
2024-07-12 13:01:03 +0200

    Presentamos members en HTML
    - Queryset en views
    - Bucle en template para formatear datos

--- proyecto/members/views.py
+++ proyecto/members/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from members.models import Member
+
 def index(request):
   # Devolvemos el HTML de templates/index.html:
   return render(request, "index.html")
 
 def members(request):
+  lista_de_members = Member.objects.all()
+  context = {
+    "members": list(lista_de_members)
+  }
   # Devolvemos el HTML de templates/members/index.html:
-  return render(request, "members/index.html")
+  return render(request, "members/index.html", context=context)
