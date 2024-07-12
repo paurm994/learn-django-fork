a51b68bafdf684d901cbac92e4a4097c4e633c09
César <cesarlpb@gmail.com>
2024-07-12 12:52:09 +0200

    Añadimos campo phone en members

--- proyecto/members/models.py
+++ proyecto/members/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
+  lastname  = models.CharField(max_length=255)
+  # El teléfono es opcional, puede ser None
+  # Formato: +34 123 456 789
+  phone     = models.CharField(max_length=15, blank=True, null=True)
 
   def __str__(self) -> str:
     return f"{self.id} - {self.firstname}, {self.lastname}"
