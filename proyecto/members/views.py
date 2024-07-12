57bad1599deee1720ba51ace25fd188eef91ef99
César <cesarlpb@gmail.com>
2024-07-12 12:19:44 +0200

    Registramos app members en settings

--- proyecto/members/views.py
+++ proyecto/members/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+  # Devolvemos el HTML de templates/index.html:
   return render(request, "index.html")
 
 def members(request):
-  # Devolvemos HTML por archivo:
+  # Devolvemos el HTML de templates/members/index.html:
   return render(request, "members/index.html")
