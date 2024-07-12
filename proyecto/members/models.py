27e69e21f6d51f5f45512c1415560bdf44b95d1c
César <cesarlpb@gmail.com>
2024-07-12 12:37:34 +0200

    Creamos modelo Member
    - Hacemos la migración
    - Añado método __str__ para representar el modelo en print

--- proyecto/members/models.py
+++ proyecto/members/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+
+  def __str__(self) -> str:
+    return f"{self.id} - {self.firstname}, {self.lastname}"
