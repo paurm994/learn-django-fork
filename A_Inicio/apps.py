4af9c6faca050334119df96c3801f8b6674a7c9e
Pau Roqué <paurm994@gmail.com>
2024-07-31 11:46:59 +0200

    eliminar cosas que no uso

--- A_Inicio/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AInicioConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "A_Inicio"
--- A_Inicio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from A_Inicio.views import index, login
-
-urlpatterns = [
-    path("", index, name="index"),
-    path("login/", login, name="login"),
-]
